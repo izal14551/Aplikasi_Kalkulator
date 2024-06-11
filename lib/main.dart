f6bcd18f6de57f26e331c45065b346aee4eedc6f
Nandarose11 <rosen9350@gmail.com@gmai.com>
2024-06-12 06:58:42 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SimpleCalculator'),
+        ),
+        body: const Padding(
+          padding: EdgeInsets.all(18.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: CalcButton(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CalcButton extends StatefulWidget {
+  const CalcButton({Key? key}) : super(key: key);
+
+  @override
+  CalcButtonState createState() => CalcButtonState();
+}
+
+class CalcButtonState extends State<CalcButton> {
+  double? _currentValue = 0;
+  @override
+  Widget build(BuildContext context) {
+    var calc = SimpleCalculator(
+      value: _currentValue!,
+      hideExpression: false,
+      hideSurroundingBorder: true,
+      autofocus: true,
+      onChanged: (key, value, expression) {
+        setState(() {
+          _currentValue = value ?? 0;
+        });
+        if (kDebugMode) {
+          print('$key\t$value\t$expression');
+        }
+      },
+      onTappedDisplay: (value, details) {
+        if (kDebugMode) {
+          print('$value\t${details.globalPosition}');
+        }
+      },
+      theme: const CalculatorThemeData(
+        borderColor: Colors.black,
+        borderWidth: 2,
+        displayColor: Color.fromARGB(255, 236, 231, 231),
+        displayStyle: TextStyle(fontSize: 80, color: Colors.yellow),
+        expressionColor: Color.fromARGB(255, 80, 65, 25),
+        expressionStyle: TextStyle(fontSize: 20, color: Colors.white),
+        operatorColor: Color.fromARGB(255, 240, 219, 31),
+        operatorStyle: TextStyle(fontSize: 30, color: Colors.white),
+        commandColor: Color.fromARGB(255, 148, 115, 6),
+        commandStyle: TextStyle(fontSize: 30, color: Colors.white),
+        numColor: Color.fromARGB(255, 56, 49, 49),
+        numStyle: TextStyle(fontSize: 50, color: Colors.white),
+      ),
+    );
+    return OutlinedButton(
+      child: Text(_currentValue.toString()),
+      onPressed: () {
+        showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (BuildContext context) {
+              return SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: calc);
+            });
+      },
+    );
+  }
+}
